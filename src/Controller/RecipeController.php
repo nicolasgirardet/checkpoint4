1f16b69a085768770488027f1b2423507fdc9b95
Nicolas Girardet <nicolasgirardet@live.fr>
2022-07-19 22:48:54 +0200

    everything is ready to start developping the website now

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,20 @@
+<?php
+
+// src/Controller/RecipeController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/', name: 'recipe_index')]
+    public function index(): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'website' => 'Wild Series',
+         ]);
+    }
+}
